ea0a18910c34da13cd670f3c2b971b64869a06fb
oldzhg <zcy000321@gmail.com>
2019-12-18 17:04:10 +0800

    add 1031.cpp

---
+++ Basic Level/1032.cpp
@@ -0,0 +1,12 @@
+//
+// Created by oldzhg on 2019-12-18.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
---
+++ Basic Level/1034.cpp
@@ -0,0 +1,11 @@
+//
+// Created by oldzhg on 2019-12-18.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
